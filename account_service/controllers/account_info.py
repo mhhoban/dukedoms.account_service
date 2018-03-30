1a684b510e9c6562081be150f4f1ab75d4c7bf0f
mhhoban <mhhoban@gmail.com>
2018-03-30 00:53:53 -0700

    implemented get ids and account info endpoints

--- account_service/controllers/account_info.py
+++ account_service/controllers/account_info.py
@@ -6,12 +6,21 @@ from flask_api import status
 from account_service.shared.db import session
 from account_service.models.account import Account
 
-def get_player_info():
+from swagger_server.models.account_info import (
+    AccountInfo
+)
+
+def get_player_info(accountId):
     """
     endpoint for getting player account info
     """
-    player_id = request.json()['playerId']
+    player_id = accountId
 
+    try:
+        account = retrieve_account_from_db(player_id)
+        return populate_account_info(account), status.HTTP_200_OK
+    except ValueError:
+        return status.HTTP_404_NOT_FOUND
 
 def get_account_ids():
     """
@@ -37,3 +46,27 @@ def retrieve_account_id_from_db(email):
         return account.id
     else:
         raise ValueError
+
+def retrieve_account_from_db(id):
+    """
+    lookup and return full account from db
+    """
+    account = session.query(Account).filter(Account.id == id).first()
+    if account:
+        return account
+    else:
+        raise ValueError
+
+def populate_account_info(account):
+    """
+    populate swagger account info object with db account model data
+    """
+    account_info = AccountInfo(
+        email=account.email,
+        account_id=account.id,
+        active_player_ids=account.active_player_ids,
+        pending_player_ids=account.pending_player_ids,
+        game_invitations=account.game_invitations
+    )
+
+    return account_info.to_dict()
