9d21dfe3d4672b4c3224a74a1bc906b9fb34afa4
mhhoban <mhhoban@gmail.com>
2018-04-09 19:54:17 -0700

    combined 'fetch api' and 'swagger setup' into single bash run

--- get_api_schemas.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/mhhoban/dukedoms.account_service_spec/master/dukedoms_account_service_api.yaml -O
-
-mv dukedoms_account_service_api.yaml account_service/swagger/account_service_api.yaml
--- swagger_setup.sh
+++ swagger_setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/mhhoban/dukedoms.account_service_spec/master/dukedoms_account_service_api.yaml -O
+
+mv dukedoms_account_service_api.yaml account_service/swagger/account_service_api.yaml
+
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i \
 ./local/account_service/swagger/account_service_api.yaml -l python-flask -o /local/swagger_codegen
 
