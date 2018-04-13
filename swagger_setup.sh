5c1b068524c0c06ba08417e6ccd25a7d4630f915
mhhoban <mhhoban@gmail.com>
2018-04-12 18:10:00 -0700

    updated swagger setup file for a saner build process

--- swagger_setup.sh
+++ swagger_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+rm -rvf swagger_codegen
 curl https://raw.githubusercontent.com/mhhoban/dukedoms.account_service_spec/master/dukedoms_account_service_api.yaml -O
 
 mv dukedoms_account_service_api.yaml account_service/swagger/account_service_api.yaml
 
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate -i \
 ./local/account_service/swagger/account_service_api.yaml -l python-flask -o /local/swagger_codegen
-
-cp -r swagger_codegen/swagger_server ./account_service_venv/lib/python3.6/site-packages/
+cd swagger_codegen
+python3 setup.py bdist_wheel
+cd ..
+pip3 install swagger_codegen/dist/swagger_server-1.0.0-py3-none-any.whl
